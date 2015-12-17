e1ab26cb43772b1eb65198d764301022a117f078
doutib <thibault.doutre@gmail.com>
2015-12-17 11:34:43 -0800

    add tests dir

--- Update.R
+++
@@ -1,9 +0,0 @@
-
-## Updating step
-
-update_grid <- function(x,x_add)
-{
-  i <- sum(x_add > x)
-  x_new <- c(x[1:i],x_add,x[(i+1):length(x)])
-  return(x_new)
-}
--- ars.Rcheck/00_pkg_src/ars/tests/testthat/test-abscissae.R
+++
@@ -1,6 +0,0 @@
-library(ars)
-context("abscissae test")
-
-test_that ("invalid domain", {
-  expect_warning(abscissae(function(x) log(dnorm(x, mean = 0, sd = 1)), c(Inf, -Inf)))
-})
